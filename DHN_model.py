d4e7e39912b9d4b807218ea25724947a428f9c35
Itreek <82515521+Itreek@users.noreply.github.com>
2022-12-05 16:42:23 +0530

    Update DHN_model.py

--- DHN_model.py
+++ DHN_model.py
@@ -2,34 +2,31 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-# approximation of DHN
-
 
 class DHN(nn.Module):
 
     # Initializing all variables
-    def __init__(self, element_dim, hidden_dim, target_size, bidirectional, minibatch, is_train=True, sigmoid=True):
+    def __init__(self, num_input_features, num_hidden_features, final_num_features, bidirectional, minibatch, is_train=True):
 
         super(DHN, self).__init__()
 
         # Initializing the parameters
-        self.hidden_dim = hidden_dim
-        self.bidirect = bidirectional
+        self.num_hidden_features = num_hidden_features
+        self.is_bidirect = bidirectional
         self.minibatch = minibatch
-        self.sigmoid = sigmoid
 
         # Initializing the layers
         self.lstm_row = nn.LSTM(
-            element_dim, hidden_dim, bidirectional=self.bidirect, num_layers=2, dropout=0.2)
+            num_input_features, num_hidden_features, bidirectional=self.is_bidirect, num_layers=2, dropout=0.2)
         self.lstm_col = nn.LSTM(
-            hidden_dim*2, hidden_dim, bidirectional=self.bidirect, num_layers=2, dropout=0.2)
+            num_hidden_features*2, num_hidden_features, bidirectional=self.is_bidirect, num_layers=2, dropout=0.2)
 
-        if self.bidirect:
-            self.hl1 = nn.Linear(hidden_dim * 2, 256)
+        if self.is_bidirect:
+            self.hl1 = nn.Linear(num_hidden_features * 2, 256)
             self.hl2 = nn.Linear(256, 64)
-            self.hl3 = nn.Linear(64, target_size)
+            self.hl3 = nn.Linear(64, final_num_features)
         else:
-            self.hl1 = nn.Linear(hidden_dim, target_size)
+            self.hl1 = nn.Linear(num_hidden_features, final_num_features)
 
         # Initializing the hidden state
         self.hidden_row = self.init_hidden(1)
@@ -46,30 +43,34 @@ class DHN(nn.Module):
                     # 1st Bi-RNN
 
                     torch.nn.init.orthogonal_(m.w_l1.data)
-                    m.bias_ih_l0.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_ih_l0.data[0:self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l1_h.data)
-                    m.bias_hh_l0.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_hh_l0.data[0:self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l1_reverse.data)
-                    m.bias_ih_l0_reverse.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_ih_l0_reverse.data[0:
+                                              self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l1_h_reverse.data)
-                    m.bias_hh_l0_reverse.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_hh_l0_reverse.data[0:
+                                              self.num_hidden_features].fill_(-1)
 
                     # 2nd Bi-RNN
 
                     torch.nn.init.orthogonal_(m.w_l2.data)
-                    m.bias_ih_l1.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_ih_l1.data[0:self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l2_h.data)
-                    m.bias_hh_l1.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_hh_l1.data[0:self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l2_reverse.data)
-                    m.bias_ih_l1_reverse.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_ih_l1_reverse.data[0:
+                                              self.num_hidden_features].fill_(-1)
                     torch.nn.init.orthogonal_(m.w_l2_h_reverse.data)
-                    m.bias_hh_l1_reverse.data[0:self.hidden_dim].fill_(-1)
+                    m.bias_hh_l1_reverse.data[0:
+                                              self.num_hidden_features].fill_(-1)
 
     def init_hidden(self, batch):
-        if self.bidirect:
-            hidden = np.zeros(2*2, batch, self.hidden_dim)
+        if self.is_bidirect:
+            hidden = np.zeros(2*2, batch, self.num_hidden_features).cuda()
         else:
-            hidden = np.zeros(2, batch, self.hidden_dim)
+            hidden = np.zeros(2, batch, self.num_hidden_features).cuda()
 
         return hidden
 
@@ -83,7 +84,7 @@ class DHN(nn.Module):
         # 1st Bi-RNN
 
         # flatten the matrix row-wise
-        flattened_row = X.view(X.size(0), -1, 1).permute(1, 0, 2).contiguous()
+        flattened_row = X.view(X.size(0), -1, 1).contiguous()
         bi_rnn_1_out, self.hidden_row = self.lstm_row(
             flattened_row, self.hidden_row)
 
@@ -92,7 +93,7 @@ class DHN(nn.Module):
         bi_rnn_1_out = bi_rnn_1_out.view(X.size(1), X.size(2), X.size(0), -1)
 
         # 2nd Bi-RNN
-        flattened_col = bi_rnn_1_out.permute(1, 0, 2, 3).contiguous()
+        flattened_col = bi_rnn_1_out.contiguous()
         flattened_col = flattened_col.view(-1, flattened_col.size(2),
                                            flattened_col.size(3)).contiguous()
         bi_rnn_2_out, self.hidden_col = self.lstm_col(
@@ -108,9 +109,7 @@ class DHN(nn.Module):
         fc_layer2 = self.hl2(fc_layer1)
         final = self.hl3(fc_layer2).view(-1, X.size(0))
 
-        if self.sigmoid:
-            tag_scores = np.sigmoid(final)
-        else:
-            tag_scores = final
+        # sigmoid activation for output of fully connected layers
+        tag_scores = np.sigmoid(final)
 
         return tag_scores.view(X.size(1), X.size(2), -1).permute(2, 0, 1).contiguous()
